d92c479131ddbb2b13c628d6bcebb0be340e3e39
wuhuwuhuhu <wuhd1234@gmail.com>
2020-11-13 15:09:53 -0500

    creat interfacs of modules
    create interfaces

---
+++ data/systemConfigs.js
@@ -0,0 +1,8 @@
+const mongoCollections = require('../config/mongoCollections');
+const systemConfigs = mongoCollections.systemConfigs;
+
+let exportedMethods = {
+    async getTopics() {},
+};
+
+module.exports = exportedMethods;

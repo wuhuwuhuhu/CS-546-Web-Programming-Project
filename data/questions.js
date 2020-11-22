935b33c6f026825212e736c450894b71d9caa580
Ying Liu <yingliu928@gmail.com>
2020-11-21 20:10:34 -0500

    updated questions.js

--- data/questions.js
+++ data/questions.js
@@ -1,6 +1,10 @@
+const reviews = require('../../Lab6/data/reviews');
 const mongoCollections = require('../config/mongoCollections');
 const questions = mongoCollections.questions;
+const answers = mongoCollections.anwsers;
 const users = mongoCollections.users;
+const reviews = mongoCollections.reviews;
+const usersData = require('./users')
 
 
 //helper method--parse id to objectId
@@ -16,8 +20,29 @@ async function myDBfunction(id) {
 }
 
 let exportedMethods = {
-    async getAllQuestions() { },
-    async getQuestionById(id) { },
+    async getAllQuestions() {
+        const questionCollection = await questions();
+        const questionList = questionCollection.find({}).toArray();
+        //change objectId to string id
+        for (let i = 0; i < questionList.length; i++) {
+            questionList[i]._id = questionList[i]._id.toString()
+        }
+        return questionList;
+    },
+    async getQuestionById(id) {
+        if(!id) throw 'questions.js|getQestionById(): you need to provide an id.'
+        if(typeof id !== 'string' || id.trim() === '') throw 'questions.js|getQestionById(): id must be non-empty string'
+
+        const questionCollection = await questions();
+        const ObjectId = await myDBfunction(id);
+        const find = questionCollection.findOne({_id: ObjectId});
+        if(find == null) throw 'questions.js|getQestionById():question not found';
+        //change objectId to string id
+        const result = Object.assign({},find);
+        result._id = find._id.toString();
+
+        return result;
+    },
     async addQuestion(content, topic, questioner) {
         if (!content || !topic || !questioner) {
             throw 'You need to provide all the question fields'
@@ -53,12 +78,20 @@ let exportedMethods = {
         let newId = insertInfo.insertedId.toString()
 
         //add question Id to user.questions[]
-        let userObjectId = await myDBfunction(questioner);
-        const userCollection = await users();
-        const updatedInfo = await userCollection.updateOne({ _id: userObjectId }, { $addToSet: { questions: newId } })
-        if (updatedInfo.matchedCount === 0 || updatedInfo.modifiedCount === 0) {
-            throw 'No user being updated.'
+       // let userObjectId = await myDBfunction(questioner);
+        // const userCollection = await users();
+        // const updatedInfo = await userCollection.updateOne({ _id: userObjectId }, { $addToSet: { questions: newId } })
+        // if (updatedInfo.matchedCount === 0 || updatedInfo.modifiedCount === 0) {
+        //     throw 'No user being updated.'
+        // }
+        try {
+            const updatedUser = await usersData.addQuestion(questioner,newId);
+        } catch (error) {
+
+            console.log(error);
+            throw 'questions.js|addQuestion(): could not update user';
         }
+
         //change id to string for output use
         let result = Object.assign({}, newQuestion);
         result['_id'] = newId;
@@ -71,9 +104,38 @@ let exportedMethods = {
 
     },
     async removeQuestion(id) {
-        //also remove comments related to the Question.
+        if(!id) throw 'questions.js|removeQuestion(): you need to provide an id'
+        if(typeof id !== 'string' || id.trim()==='') throw 'questions.js|removeQuestion(): id must be non-empty string'
+
+        const question = this.getQuestionById(id);
+        if(question == null) throw `questions.js|removeQuestion(): question with ${id} not found`
+      //  remove answers first
+        if(question.answers.length !==0 ){
+            //loop the answers array to delete all the related answers;
+            for(let answerId of question.answers){
+                const answerCollection = await answers();
+                const answerObjectId = await myDBfunction(answerId);
+                const findAnswer = await answerCollection.findOne({_id:answerObjectId});
+                if(findAnswer == null) throw 'questions.js|removeQuestion():answer not found';
+                // remove related reviews
+                if(findAnswer.reviews.length !== 0){
+                    let reviewsList = findAnswer.reviews;
+                    for(let reviewId of reviewsList){
+                        const reviewCollection = await reviews();
+                        let reviewObjectId = await myDBfunction(reviewId);
+                        const deleteInfo = await reviewCollection.deleteOne({_id:reviewObjectId});
+
+                    }
+                }
+                const deleteInfo = answerCollection.deleteOne({_id:answerObjectId})
+                if(deleteInfo.deletedCount === 0) throw 'questions.js|removeQuestion(): no answers been deleted.'
+
+            }
+        }
     },
-    async updateQuestion(id, content, topic) { },
+    async updateQuestion(id, content, topic) {
+      
+     },
 
     async addAnswer(id, answerId) { },
     async removeAnswer(id, answerId) { },

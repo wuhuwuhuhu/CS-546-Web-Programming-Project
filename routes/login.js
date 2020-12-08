ec58ed9ab3e70fbda87a04a45a4e165ebb6ff228
PeijinZhou <71521728+PeijinZhou@users.noreply.github.com>
2020-12-07 20:19:54 -0500

    fetch

---
+++ routes/login.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const userData = require('../data/users');
+const xss = require('xss');
+
+if(req.session.userInfo){
+    let { userInfo } = req.session
+    res.render("pages/home", { userInfo: userInfo })
+  }else{
+    res.render("pages/login", {})
+  };
+
+router.get("/regist", async (req, res) => {
+  res.render("pages/register")
+})
+router.post("/regist", async (req, res) => {
+
+  let { username, password } = req.body
+  if (username == "" || username.length < 5) {
+    res.render("pages/register", { error: "please check username length >5 and not empty" })
+    return
+  }
+  if (password == "" || password.length < 5) {
+    res.render("pages/register", { error: "please check password length >5  and not empty" })
+    return
+  }
+  if(email == ""){
+    res.render("pages/register", { error: "please check email not empty" })
+    return
+  }
+  let list = userList.filter(item => item.username == username)
+  if (list.length > 0) {
+    res.render("pages/register", { error: "The user name is already registered" })
+    return
+  } else {
+    userList.push(req.body)
+    res.json({ status: 1, msg: "regist success" })
+    return
+  }
+})
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body
+  let list = userList.filter(item => item.username == username)
+  if (list.length > 0) {
+    if (list[0].password == password) {
+      req.session.userInfo = list[0]
+      res.render("pages/home", { userInfo: list[0] })
+    } else {
+      res.render("pages/login", { error: "Please check that the user name and password are correct " })
+      return
+    }
+  } else {
+    res.render("pages/login", { error: "Please check that the user name and password are correct " })
+    return
+  }
+
+})
+function islogin(req, res, next) {
+  if (req.session.userInfo) {
+    next()
+  } else {
+    res.render("pages/login", {})
+  }
+}
+
+router.get("/private", islogin, async (req, res) => {
+  let { userInfo } = req.session
+  res.render("pages/home", { userInfo: userInfo })
+})
+router.get("/logout", islogin, async (req, res) => {
+  delete req.session.userInfo
+  res.json({ status: "1", msg: "success" })
+})
+
+module.exports = router;
+

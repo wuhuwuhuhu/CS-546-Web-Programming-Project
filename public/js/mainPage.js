285f212fb13c8eca8107fd4fdf1afcb834c67bfb
PeijinZhou <71521728+PeijinZhou@users.noreply.github.com>
2020-12-12 17:14:02 -0500

    Revert "fix"
    This reverts commit d3dcf4ae3f9c5c992a86fe05969a7281cf362708.

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,77 +1,44 @@
-showMostPopularItem()
-
-$('#timeButton').click((event)=>{
-    event.preventDefault();
-    $('#Myform').hide();
-    $('#searchResult').hide();
-    $('#popularQuestion').hide();
-    $('#myNewForm').show();
-    $("#newSearchResult").show();
-    $('#newPopularQuestion').show();
-    $('#myHotForm').hide();
-    $('#hotSearchResult').hide();
-    $('hotPopularQuestion').hide();
-})
-
-showMostPopularItem()
 $('#Myform').submit((event)=>{
     event.preventDefault();
-    $('#error-container').hide();
+    $('#popularQuestion').hide();
     if($('#text_input').val()){
-        $('#popularQuestion').hide();
         $('#searchResult').children().remove();
+        $('#searchResult').show();
         let searchAnswer = search($('#text_input').val());
         console.log(searchAnswer);
-    }
-    else{
-        $('#error-container').show();
-        let tag ="<a>You must enter some words to search!!</a>"
-        $('#error-container').append(tag)
-
-    }
-});
-
-
-$('#myNewForm').submit((event)=>{
-    event.preventDefault();
-    $('#error-container').hide();
-    if($('#text_input2').val()){
-        $('#newPopularQuestion').hide();
-        $('#newSearchResult').children().remove();
-        let searchAnswer = searchNew($('#text_input2').val());
-        console.log(searchAnswer);
-    }
-    else{
-        $('#error-container').show();
-        let tag ="<a>You must enter some words to search!!</a>"
-        $('#error-container').append(tag)
-
-    }
-});
-
-function showMostPopularItem(){
-    $('#searchResult').hide();
-    $('#popularQuestion').children().remove();
-    let mostPopular = {
-        method:'POST',
-        url:'/popular',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            ask:true
-        })
-    }
-    $('#popularQuestion').show()
-    $.ajax(mostPopular).then(function(responseMessage){
-        for(let i=0;i<5;i++){
-            let li = '<li><a href=question/'+responseMessage.returnPopular[i]._id+' >'+responseMessage.returnPopular[i].content+'</a></li>';
-            $('#popularQuestion').append(li);
+        for(let i=0;i<searchAnswer.length;i++){
+            let li = '<li href='+searchAnswer[i][url]+' >'+searchAnswer[i][title]+'</li>';
+            $('#searchResult').append(li);
+            $('text_input').focus();
         }
-        
+    }
+    else{
+        alert("You must input something to search!")
+    }
+});
+let mostPopular = {
+    method:'POST',
+    url:'/popular',
+    contentType: 'application/json',
+    data: JSON.stringify({
+        ask:true
     })
 }
+$('#popularQuestion').show()
+$.ajax(mostPopular).then(function(responseMessage){
+    for(let i=0;i<5;i++){
+        let li = '<li><a href='+responseMessage.returnPopular[i].url+' >'+responseMessage.returnPopular[i].name+'</a></li>';
+        $('#popularQuestion').append(li);
+    }
+    
+})
+
+
+
+
+
 
 function search(item){
-    $('#searchResult').show();
     let search ={
         method:'POST',
         url:'/search',
@@ -82,38 +49,12 @@ function search(item){
     }
 
     $.ajax(search).then(function(responseMessage){
-        if(responseMessage.returnSearch.length==0){
-            $('#error-container').children().remove();
-            $('#error-container').show();
-            $('#error-container').append("<p>No search answer!!</p>")
-        }
-        else{
-            for(i=0;i<responseMessage.returnSearch.length;i++){
-                let li ='<li><a href=question/'+responseMessage.returnSearch[i]._id+'>'+responseMessage.returnSearch[i].content+'</a></li>' ;
-                $('#searchResult').append(li);
-            }
+        for(i=0;i<responseMessage.returnSearch.length;i++){
+            let li ='<li><a href='+responseMessage.returnSearch[i].url+'>'+responseMessage.returnSearch[i].name+'</a></li>' ;
+            $('#searchResult').append(li);
         }
     })
 
     //must return a Array of searchAnswer, the search answer must be a object, key:url and key:title(must be string);
 
-}
-function searchNew(item){
-    $('#newSearchResult').show();
-    let search ={
-        method:'POST',
-        url:'/searchNew',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            data: item
-        })
-    }
-
-    $.ajax(search).then(function(responseMessage){
-        console.log(responseMessage)
-        for(i=0;i<responseMessage.returnSearch.length;i++){
-            let li ='<li><a href=question/'+responseMessage.returnSearch[i]._id+'>'+responseMessage.returnSearch[i].content+'</a></li>' ;
-            $('#searchResult').append(li);
-        }
-    })
 }

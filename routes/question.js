472ce73468489b0dd7a77fd7518e64662b8c1173
louyilin <louyilin1996@gmail.com>
2020-12-12 16:04:50 -0500

    question router functions init

--- routes/question.js
+++ routes/question.js
@@ -4,6 +4,7 @@ const data = require('../data');
 const answerDate = data.answers;
 const questionsData = data.questions;
 const userData = data.users;
+const reviewDate = data.reviews;
 const session = require('express-session');
 
 
@@ -16,25 +17,83 @@ const session = require('express-session');
  */
 router.get('/:id', async (req, res) => {
     try {
-        // if(req.session.user){
-        // }
+        let userId = req.session.user
         let id = req.params.id;
         const question = await questionsData.getQuestionById(id)
         console.log(question);
         questionContent = question.content
         const answersId = question.answers
         let answersInQuestion = new Array()
-        console.log(answersId);
+        let reviewsInAnswers = new Array()
         for (let index = 0; index < answersId.length; index++) {
+            let curReviewsInAnswers = new Array()
             let curAnswerId = answersId[index];
-            answersInQuestion.push(await answerDate.getAnswerById(curAnswerId))
+            let curAnswer = await answerDate.getAnswerById(curAnswerId)
+            answersInQuestion.push(curAnswer)
+            let curReviewIdArray = curAnswer.reviews
+            for (let j = 0; j < curReviewIdArray.length; j++) {
+                let curReviewId = curReviewIdArray[j];
+                let curReview = await reviewDate.getReviewById(curReviewId)
+                curReviewsInAnswers.push(curReview)
+            }
+            reviewsInAnswers.push(curReviewsInAnswers)
         }
-        console.log(answersInQuestion);
-        res.json("done")
+        res.render('questionDetails/questionInfo.handlebars', {
+            userId: userId,
+            answersInQuestion: answersInQuestion,
+            reviewsInAnswers: reviewsInAnswers,
+        });
     } catch (error) {
         res.json("didn't find question")
     }
-});
+})
+
+
+/**
+ * add a new answer question 
+ * questionId: id of target question
+ */
+router.post('/addAnswer/:questionId', async (req, res) => {
+
+})
+
+/**
+ * add a new review to answer which under a question
+ */
+router.post('/addReview/:questionId/:answerId', async (req, res) => {
+
+})
+
+/**
+ * vote up an answer
+ */
+router.post('/voteUpAnswer/:questionId/:answerId', async (req, res) => {
+
+})
+
+/**
+ * vote down an answer
+ */
+router.post('/voteDownAnswer/:questionId/:answerId', async (req, res) => {
+
+})
+
+/**
+ * vote up an review
+ */
+router.post('/voteUpReview/:questionId/:answerId/:reviewId', async (req, res) => {
+
+})
+
+/**
+ * vote down an review
+ */
+router.post('/voteDownReview/:questionId/:answerId/:reviewId', async (req, res) => {
+
+})
+    ;
+
+
 
 
 

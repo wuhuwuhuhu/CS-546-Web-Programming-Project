0fda300b485b0904892c17907f02b0673fc01726
PeijinZhou <71521728+PeijinZhou@users.noreply.github.com>
2020-12-12 17:22:38 -0500

    main

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { sortQuestionsByTime, sortQuestionsByAnsNum, getQuestionsByKeywords } = require('../data/questions');
 const router = express.Router();
 const questions = require('../data/questions')
 const userData = require('../data/users');
@@ -10,42 +11,23 @@ router.get('/', function(req,res)  {
 });
 router.post('/search', async(req,res)=>{
     let search = req.body.data;
-    let allQuestion = questions.getAllQuestions;
+
+    let searchQuestion = await getQuestionsByKeywords(search);
     // get the name for all question , using loop to return 
-    let A = {returnSearch:[{url:'/20',name:'wtf1'},{url:'/1',name:'wtf2'},{url:'/1',name:'wtf3'}]};
+    console.log(search)
+    console.log(searchQuestion)
+    let A = {returnSearch:searchQuestion};
     res.json(A);
 })
 
 router.post('/popular',async(req,res)=>{
     if(req.body.ask==true){
-        let A = {returnPopular:[{url:'/201',name:'enen1'},{url:'/123',name:'enen2'},{url:'/172',name:'enen3'},{url:'/199',name:'enen4'},{url:'/188',name:'enen5'}]};
+        let allQuestionSort =await sortQuestionsByAnsNum(await questions.getAllQuestions(), 20)
+        // get the name for all question , using loop to return 
+    
+        let A = {returnPopular:allQuestionSort};
         res.json(A);
     }
 })
 
-function search_word(text, word){
-    
-    let x = 0, y=0;
-   
-    for (i=0;i< text.length;i++)
-        {
-        if(text[i] == word[0])
-            {
-            for(j=i;j< i+word.length;j++)
-               {
-                if(text[j]==word[j-i])
-                  {
-                    y++;
-                  }
-                if (y==word.length){
-                    x++;
-                }
-            }
-            y=0;
-        }
-    }
-    if(x!=0){
-        return true;
-   }
-}
 module.exports = router;

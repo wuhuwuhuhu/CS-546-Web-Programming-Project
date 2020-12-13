07ab9511e6ba612f0bfa657c1593dcb3c28a4126
louyilin <louyilin1996@gmail.com>
2020-12-12 23:24:39 -0500

    question page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const userRoutes = require("./users");
 const askRoutes = require('./ask')
-const mainRoutes = require('./main')
-
+const loginRoutes = require('./login');
+const mainRoutes = require('./main');
+const questionRoutes = require('./question')
+// const regist = require("./registration")
+// const logout = require("./logout")
 const constructorMethod = (app) => {
     app.use('/user', userRoutes);
     app.use('/ask', askRoutes);
+    app.use('/question', questionRoutes);
+    // app.use(regist);
+    app.use('/login',loginRoutes);
+    // app.use('/logout',logout);
     app.use('/',mainRoutes)
     app.use("*", (req, res) => {
         res.sendStatus(400);

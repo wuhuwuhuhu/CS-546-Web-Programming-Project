d2b9f05b39535ed657b05424e2eef6487a6640cb
louyilin <louyilin1996@gmail.com>
2020-12-13 17:20:58 -0500

    Merge branch 'main' of https://github.com/wuhuwuhuhu/CS-546-Web-Programming-Project into main

--- public/js/questionDetails.js
+++ public/js/questionDetails.js
@@ -1,13 +1,26 @@
 (function ($) {
     const answerSubmit = $('#answerSubmit')
-    const answerContent=$('#answerContent')
-    const quesId=$('#quesId')
-
+    const answerContent = $('#answerContent')
+    const quesId = $('#quesId')
+    const mainTable = $('mainTable')
     answerSubmit.click(function (event) {
         event.preventDefault();
-        var answer = answerContent.val();
-        console.log(answer);
-        console.log(quesId.text());
+        var userId = $.session.get('key');
         
+        console.log(userId);
+        var content = answerContent.val();
+        const questionId=quesId.text()
+        var requestConfig = {
+            method: 'POST',
+            url: '/',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                content: content,
+                questionId: questionId,
+
+            })
+        };
+
+
     });
 })(jQuery);

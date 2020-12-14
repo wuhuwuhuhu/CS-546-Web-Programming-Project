89f3d2e2979caf5088e1818250956d72da33c63a
yingliu928 <yingliu928@gmail.com>
2020-12-14 18:45:25 -0500

    updated form

--- public/js/askForm.js
+++ public/js/askForm.js
@@ -4,16 +4,22 @@
 	var askBtn = $('#ask-submit-btn');
 	var inputErrorDiv1 = $('#ask-error-div1');
 	var inputErrorDiv2 = $('#ask-error-div2');
+	var inputErrorDiv3 = $('#ask-error-div3');
 	var remainp = $('#question-remain')
+	var select = $('#select-topic option')
 	askForm.on('submit',function(event){
-		
+		if(select.filter(':selected').val() == ''){
+			event.preventDefault()
+			askInput.focus();
+			inputErrorDiv3.show()
+		}
 		if(!askInput.val() || askInput.val().trim() === '' || askInput.val().trim().length <5){
 			event.preventDefault()
 			askInput.focus();
 			inputErrorDiv1.show()
 	
 		}
-		
+
 		
 	})
 
@@ -22,6 +28,7 @@
 		//get the maxlength attr
 		inputErrorDiv1.hide()
 		inputErrorDiv2.hide()
+		inputErrorDiv3.hide()
 		var maxlength = $(this).attr('maxlength');
 		var val = $(this).val().length;
 		remain = parseInt(maxlength - val);

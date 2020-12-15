ab0dbd3d4cedba11268e1df9d2d6918bea5db359
wuhuwuhuhu <wuhd1234@gmail.com>
2020-12-15 01:16:18 -0500

    finished login/register/reset pages

--- routes/reset.js
+++ routes/reset.js
@@ -2,65 +2,53 @@ const express = require('express');
 const router = express.Router();
 const userData = require('../data/users');
 const xss = require('xss');
+const sendEmail = require("../data/email").send;
 
 router.get("/", async (req, res) => {
-    res.render('login/login', {
-      title: "Log in"
+    res.render('login/reset', {
+      title: "Reset Password"
     });
   })
   router.post("/", async (req, res) => {
   
-    let { email, password } = req.body;
+    let { email } = req.body;
     email = xss(email).toLowerCase();
-    password = xss(password);
     let error_msgs = [];
     let status = false;
+    const newPassword = "abcd1234";
     let user;
-    let passwordMatch;
   
       if (!email) {
           error_msgs.push("Must provide valid email.");
       }
   
-      if (!password) {
-          error_msgs.push("Must provide valid password.")
-          }
       if(error_msgs.length === 0){
           let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
           if(!emailPattern.test(email)){
                   error_msgs.push("You must provide a valid email.");
               }
-          if(password.length<3 || password.length >16){
-              error_msgs.push("Please user 3-16 characters long password.");
-          }
       }
       if(error_msgs.length === 0){
           try {
-            //   user = await userData.checkPassword(
-            //       email, 
-            //       password
-            //   );
-
-            //for test 
-            const bcrypt = require('bcryptjs');
+            // user =  await user.setPassword(email, newPassword);
             user = await userData.getUserByEmail(email);
-            passwordMatch;
-            if(user){
-                try {
-                    passwordMatch = await bcrypt.compare(password, user.hashedPassword);
-                } catch (e) {
-                    console.log(e);
-                }
-            }
-            
           } catch (error) {
               error_msgs.push(error.message);
           }
       }
-      if(error_msgs.length === 0 && passwordMatch === true){
+      if(error_msgs.length === 0){
           status = "true";
           if(user){
-              req.session.user = user["_id"].toString();
+
+            let userName = user["userName"];
+            let userEmail = user["email"];
+            let subject = `Hi ${userName}, Your password has been reset to ${newPassword}.`;
+            let text = `Hi ${userName},\n Your password has been reset to ${newPassword}.\n thanks.`;
+            try {
+                await sendEmail(userEmail, subject, text);
+            } catch (error) {
+                console.log(error);
+            }
           }
       }
   
